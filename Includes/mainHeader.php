6470893c42bbdd7e6804865d5d2e477e881eb898
ZohaibYunis <zohaib.yunis@gmail.com>
2019-06-25 06:05:17 -0700

    creating partial template

---
+++ Includes/mainHeader.php
@@ -0,0 +1,30 @@
+<div class="header-main">
+    <div class="logo-w3-agile">
+        <h1><a href="index.php">Team App</a></h1>
+    </div>
+
+    <div class="profile_details w3l" style="float: right;">		
+        <ul>
+            <li class="dropdown profile_details_drop">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <div class="profile_img">	
+                        <span class="prfil-img"><img src="images/in4.png" alt=""> </span> 
+                        <div class="user-name">
+                            <p>Admin</p>
+                            <span>Administrator</span>
+                        </div>
+                        <i class="fa fa-angle-down"></i>
+                        <i class="fa fa-angle-up"></i>
+                        <div class="clearfix"></div>	
+                    </div>	
+                </a>
+                <ul class="dropdown-menu drp-mnu">
+                    <li> <a href="#"><i class="fa fa-sign-out"></i> Logout</a> </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+    <div class="clearfix"> </div>	
+</div>
+
